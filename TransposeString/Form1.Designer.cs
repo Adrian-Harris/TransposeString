66696ce9483d902bd6c7222308da221df1724d78
Adrian-Harris <adrianharris@msn.com>
2022-01-23 19:53:03 +1000

    altered to run from the system tray exclusively

--- TransposeString/Form1.Designer.cs
+++ TransposeString/Form1.Designer.cs
@@ -28,26 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(284, 214);
-            this.textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(11, 264);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(42, 23);
             this.button1.TabIndex = 1;
@@ -57,7 +51,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(59, 263);
+            this.button2.Location = new System.Drawing.Point(60, 11);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(52, 23);
             this.button2.TabIndex = 2;
@@ -67,7 +61,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(117, 264);
+            this.button3.Location = new System.Drawing.Point(118, 12);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(42, 23);
             this.button3.TabIndex = 3;
@@ -75,16 +69,9 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(12, 234);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(284, 23);
-            this.textBox2.TabIndex = 4;
-            // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(59, 294);
+            this.button4.Location = new System.Drawing.Point(60, 42);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(52, 23);
             this.button4.TabIndex = 5;
@@ -94,7 +81,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(12, 293);
+            this.button5.Location = new System.Drawing.Point(13, 41);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(42, 23);
             this.button5.TabIndex = 6;
@@ -102,33 +89,42 @@
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Transpose String";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(305, 329);
+            this.ClientSize = new System.Drawing.Size(255, 80);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "TransposeString";
+            this.Shown += new System.EventHandler(this.Form_Shown);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private TextBox textBox1;
         private Button button1;
         private Button button2;
         private Button button3;
-        private TextBox textBox2;
         private Button button4;
         private Button button5;
+        private NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuStrip1;
     }
 }
--- TransposeString/Form1.cs
+++ TransposeString/Form1.cs
@@ -3,11 +3,20 @@ namespace TransposeString
     public partial class Form1 : Form
     {
 
-        String[] lines;
+        private void Form_Shown(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+            this.Hide();
+        }
 
         public Form1()
         {
             InitializeComponent();
+            this.notifyIcon1.ContextMenuStrip.Items.Add(button1.Text, null, button1_Click);
+            this.notifyIcon1.ContextMenuStrip.Items.Add(button5.Text, null, button5_Click);
+            this.notifyIcon1.ContextMenuStrip.Items.Add(button2.Text, null, button2_Click);
+            this.notifyIcon1.ContextMenuStrip.Items.Add(button4.Text, null, button4_Click);
+            this.notifyIcon1.ContextMenuStrip.Items.Add(button3.Text, null, button3_Click);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -39,13 +48,13 @@ namespace TransposeString
 
         void splitLines(string startchars, string midchars, string endchars)
         {
+            string[] lines;
+            string InString;
+
             try
             {
-
-                if (textBox1.Text.Length == 0)
-                {
-                    textBox1.Text = Clipboard.GetText();
-                }
+                InString = Clipboard.GetText();
+                
             }
             catch (Exception ex )
             {
@@ -54,7 +63,7 @@ namespace TransposeString
             }
 
             string result = startchars;
-                lines = textBox1.Text.Split(Environment.NewLine);
+                lines = InString.Split(Environment.NewLine);
                 int totalLines = lines.Count();
                 int Currentline = 0;
 
@@ -70,36 +79,19 @@ namespace TransposeString
                     result += line + midchars;
                 }
                     Currentline++;
-
-
                 }
 
                 result += endchars;
-                textBox2.Text = result;
 
             try
             {
                 Clipboard.SetText(result);
-                
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
                 return;
             }
-
-            
-                
-
-          
-
-
-           
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }

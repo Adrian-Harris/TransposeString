d579b3570385c992a3e29c09895a2f29e0437edd
Adrian-Harris <adrianharris@msn.com>
2022-01-23 19:02:07 +1000

    Initial Commit

---
+++ TransposeString/Form1.cs
@@ -0,0 +1,107 @@
+namespace TransposeString
+{
+    public partial class Form1 : Form
+    {
+
+        String[] lines;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            splitLines(@"", @",", @"");
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            splitLines(@"(", @",", @")");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            splitLines(@"""", @""", """, @"""");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            splitLines(@"(""", @""", """, @""")");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            splitLines(@"", @"|", @"");
+        }
+
+
+        void splitLines(string startchars, string midchars, string endchars)
+        {
+            try
+            {
+
+                if (textBox1.Text.Length == 0)
+                {
+                    textBox1.Text = Clipboard.GetText();
+                }
+            }
+            catch (Exception ex )
+            {
+                MessageBox.Show(ex.Message);    
+                return;
+            }
+
+            string result = startchars;
+                lines = textBox1.Text.Split(Environment.NewLine);
+                int totalLines = lines.Count();
+                int Currentline = 0;
+
+                foreach (string line in lines)
+                {
+                    if (Currentline == totalLines - 1)
+                    {
+                        midchars = "";
+                    }
+
+                if (line.Trim().Length > 0)
+                {
+                    result += line + midchars;
+                }
+                    Currentline++;
+
+
+                }
+
+                result += endchars;
+                textBox2.Text = result;
+
+            try
+            {
+                Clipboard.SetText(result);
+                label1.Visible = true;
+                Thread.Sleep(500);
+                label1.Visible = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            
+                
+
+          
+
+
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
